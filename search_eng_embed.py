88783047715f584518e467dbcc574f4e0d673b95
deviprajwala <deviprajwala3@gmail.com>
2021-08-09 12:30:52 +0530

    Finds similar words

--- search_eng_embed.py
+++ search_eng_embed.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pandas 
-
+import math
 terms = []
 #list to store the terms present in the documents
 keys = []
@@ -12,7 +12,7 @@ dummy_list = []
 embeddings_dict = {}
 
 doc_average ={}
-
+similar_embeddings = []
 def read_embeddings():
     
     with open("glove.6B.50d.txt", 'r') as f:
@@ -60,7 +60,7 @@ def vectorise():
         for words in dicti[i]:
             avg += sum(embeddings_dict[words])
         #print(avg, i)
-    
+        #print(embeddings_dict[words])
         doc_average.update({i:avg})
         avg = 0
        
@@ -68,10 +68,43 @@ def vectorise():
 def get_embedding(query):
     weight = 0
     for word in query:
-        weight += sum(embeddings_dict[word])
+        weight = sum(embeddings_dict[word])
+        #print(embeddings_dict[word])
+    return embeddings_dict[word]
+    #sum or the average of embeddings can be used if the query is a sentence but here we are considering a single word as query
 
-    return weight
+def cosine_similarity( list1, list2 ):
+    numerator = 0
+    denomi1 = 0
+    denomi2 = 0
 
+    for i in range (len(list1)):
+        numerator += list1[i] * list2[i]
+        denomi1 += list1[i] * list1[i]
+        denomi2 += list2[i] * list2[i]
+        
+    denominator = math.sqrt(denomi1) * math.sqrt(denomi2)
+    simili = numerator/denominator
+
+    return simili
+
+def similar_words(query_embeding):
+    for i in embeddings_dict:
+        sim = cosine_similarity(embeddings_dict[i],query_embeding)
+
+        '''if(sim >= 0.8):
+            print(i,sim)
+        Here you can adjuct the similarity threshold and check for the similar words, the cosine similarity range is 0 to 1, in the above lines
+        of code I have considered the similarity measure to be of minimum 0.8, it can be varied as per the requirement'''
+
+
+        if( sim >= 0.8 ):
+            similar_embeddings.append(i)
+
+def clustering():
+    print(similar_embeddings)
+    print(dicti)
+        
 if __name__=="__main__":
     
     #print("hi")
@@ -101,8 +134,8 @@ if __name__=="__main__":
     #spliting the query as a list of strings
     
     query_weight = get_embedding(query)
-    print(query_weight)
-    li = []
-    print(doc_average)
-    '''for i in doc_average:
-        print(doc_average[i]-query_weight)'''
+    #print(query_weight)
+    
+
+    similar_words(query_weight)
+    clustering()
